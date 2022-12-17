ca829c1ac7bf4bae48484828551619d730961f93
milanochuang <milanochuang@gmail.com>
2022-12-18 02:21:56 +0800

    add api.py

--- src/api/api.py
+++ src/api/api.py
@@ -1,26 +1,68 @@
 from flask import Flask
-from flask_restful import Resource, Api, reqparse
-import pandas as pd
-import ast
+from flask import jsonify, request
+import json
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import jieba
+
 app = Flask(__name__)
-api = Api(app)
 
-class Users(Resource):
-    # methods go here
-    pass
-    
-class Locations(Resource):
-    # methods go here
-    pass
+with open("./data/data.json") as f:
+    full_json = json.load(f)
+    dataLIST = full_json['articles']
 
-class Users(Resource):
-    def get(self):
-        with open()  # read CSV
-        data = data.to_dict()  # convert dataframe to dictionary
-        return {'data': data}, 200  # return data and 200 OK code
+def segmentation(title):
+  all_title = []
+  for i in title:
+    cut_title = jieba.cut(i)
+    seg_title = ' '.join(cut_title)
+    all_title.append(seg_title)
 
-api.add_resource(Users, '/users')  # '/users' is our entry point for Users
-api.add_resource(Locations, '/locations')  # and '/locations' is our entry point for Locations
+  return all_title
+
+def cosine_similarity(dataLIST, new_article_title, filter):
+    titleLIST = [i['article_title'] for i in dataLIST]
+    titleLIST.insert(0, new_article_title)
+    all_title = segmentation(titleLIST)
+    tfidf_vectorizer = TfidfVectorizer()
+    tfidf_matrix = tfidf_vectorizer.fit_transform(dataLIST)
+    cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+    cosine_sim_target = cosine_sim[0].tolist()
+    title_similarity = dict()
+    for i in range(len(cosine_sim_target)):
+        title_similarity[all_title[i]] = cosine_sim_target[i]
+        target_title = sorted(title_similarity.items(), key=lambda item: item[1], reverse=True) 
+    return target_title[:filter]
+
+@app.route("/")
+def welcome():
+    return "Welcome to PTT API"
+
+@app.route("/ptt/all", methods=['GET'])
+def ptt_all():
+    return jsonify(dataLIST)
+
+@app.route("/ptt/title", methods=['GET'])
+def ptt_title():
+    titleLIST = []
+    for article in dataLIST:
+        titleLIST.append(article['article_title'])
+    return jsonify(titleLIST)
+
+@app.route("/similarity", methods=['GET'])
+def get_similarity():
+    """
+    request url: http://127.0.0.1/similarity?title={input_title}&filter={input_filter}
+    """
+    if 'title' in request.args and 'filter' in request.args:
+        new_article_title = request.args['title']
+        filter = request.args['filter']
+        similar_articles = cosine_similarity(dataLIST=dataLIST, new_article_title=new_article_title, filter=filter)
+        similar_articles_list = [similar_article[0] for similarity, similar_article in enumerate(similar_articles)]
+        return jsonify(similar_articles_list)
+
+
 
 if __name__ == '__main__':
-    app.run()  # run our Flask app
+    app.debug = False
+    app.run(host='localhost', port=5000)
